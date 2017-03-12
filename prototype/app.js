2ccc624f05014ecc24f11ce903d4eb74bb27b1cf
Ben Thomasson <bthomass@ansible.com>
2017-03-11 21:12:45 -0500

    Adding prototype

---
+++ prototype/app.js
@@ -0,0 +1,15 @@
+var app = angular.module('triangular', []);
+
+var scope;
+
+app.controller('MainCtrl', function($scope) {
+  scope = $scope
+  $scope.graph = {'width': window.innerWidth,
+                  'right_column': window.innerWidth - 100,
+                  'height': window.innerHeight}
+  $scope.circles = [
+  	{'x': 15, 'y': 20, 'r':15},
+  	{'x': 50, 'y': 60, 'r':20},
+  	{'x': 80, 'y': 10, 'r':10},
+  ]
+});
