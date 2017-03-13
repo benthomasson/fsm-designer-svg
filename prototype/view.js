aec72e0e8ba046f88adf4a701de5547b12676774
Ben Thomasson <bthomass@ansible.com>
2017-03-13 11:52:22 -0400

    Starting fsm designer template

---
+++ prototype/view.js
@@ -0,0 +1,87 @@
+var inhertits = require('inherits');
+var fsm = require('./fsm.js');
+
+
+function _Ready () {
+}
+inhertits('_Ready', fsm._State);
+var Ready = _Ready();
+exports.Ready = Ready;
+
+function _Start () {
+}
+inhertits('_Start', fsm._State);
+var Start = _Start();
+exports.Start = Start;
+
+function _Scale () {
+}
+inhertits('_Scale', fsm._State);
+var Scale = _Scale();
+exports.Scale = Scale;
+
+function _Pressed () {
+}
+inhertits('_Pressed', fsm._State);
+var Pressed = _Pressed();
+exports.Pressed = Pressed;
+
+function _Pan () {
+}
+inhertits('_Pan', fsm._State);
+var Pan = _Pan();
+exports.Pan = Pan;
+
+
+
+
+_Ready.prototype.mouseDown = function (controller) {
+
+    controller.changeState(Pressed);
+
+};
+
+_Ready.prototype.mouseWheel = function (controller) {
+
+    controller.changeState(Scale);
+
+};
+
+
+
+_Start.prototype.start = function (controller) {
+
+    controller.changeState(Ready);
+
+};
+
+
+
+_Scale.prototype.timeout = function (controller) {
+
+    controller.changeState(Ready);
+
+};
+
+
+
+_Pressed.prototype.mouseUp = function (controller) {
+
+    controller.changeState(Ready);
+
+};
+
+_Pressed.prototype.mouseMove = function (controller) {
+
+    controller.changeState(Pan);
+
+};
+
+
+
+_Pan.prototype.mouseUp = function (controller) {
+
+    controller.changeState(Ready);
+
+};
+
