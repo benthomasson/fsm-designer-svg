3e04981a6d1cca879791fb72aad917b5b39f66ab
Ben Thomasson <bthomass@ansible.com>
2017-03-14 10:22:45 -0400

    Working on move fsm

---
+++ prototype/move.js
@@ -0,0 +1,84 @@
+var inherits = require('inherits');
+var fsm = require('./fsm.js');
+
+function _State () {
+}
+inherits(_State, fsm._State);
+
+
+function _Ready () {
+    this.name = 'Ready';
+}
+inherits(_Ready, _State);
+var Ready = new _Ready();
+exports.Ready = Ready;
+
+function _Start () {
+    this.name = 'Start';
+}
+inherits(_Start, _State);
+var Start = new _Start();
+exports.Start = Start;
+
+function _Selected2 () {
+    this.name = 'Selected2';
+}
+inherits(_Selected2, _State);
+var Selected2 = new _Selected2();
+exports.Selected2 = Selected2;
+
+function _Move () {
+    this.name = 'Move';
+}
+inherits(_Move, _State);
+var Move = new _Move();
+exports.Move = Move;
+
+function _Selected1 () {
+    this.name = 'Selected1';
+}
+inherits(_Selected1, _State);
+var Selected1 = new _Selected1();
+exports.Selected1 = Selected1;
+
+
+
+
+_Ready.prototype.onMouseDown = function (controller) {
+
+    controller.changeState(Selected1);
+
+};
+
+
+
+_Start.prototype.start = function (controller) {
+
+    controller.changeState(Ready);
+
+};
+
+
+
+_Selected2.prototype.onMouseDown = function (controller) {
+
+    controller.changeState(Selected1);
+
+};
+
+
+
+
+
+_Selected1.prototype.onMouseMove = function (controller) {
+
+    controller.changeState(Move);
+
+};
+
+_Selected1.prototype.onMouseUp = function (controller) {
+
+    controller.changeState(Selected2);
+
+};
+
