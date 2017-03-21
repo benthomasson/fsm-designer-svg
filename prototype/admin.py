c6b8e39a22790d8579485659938724f985c7896b
Ben Thomasson <bthomass@ansible.com>
2017-03-21 07:43:42 -0400

    Adding models for Client, MessageType, and TopologyHistory

--- prototype/admin.py
+++ prototype/admin.py
@@ -6,6 +6,12 @@ from prototype.models import Link
 
 from prototype.models import Topology
 
+from prototype.models import Client
+
+from prototype.models import TopologyHistory
+
+from prototype.models import MessageType
+
 
 class DeviceAdmin(admin.ModelAdmin):
     fields = ('topology', 'name', 'x', 'y', 'id', 'type',)
@@ -29,3 +35,27 @@ class TopologyAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Topology, TopologyAdmin)
+
+
+class ClientAdmin(admin.ModelAdmin):
+    fields = ()
+    raw_id_fields = ()
+
+
+admin.site.register(Client, ClientAdmin)
+
+
+class TopologyHistoryAdmin(admin.ModelAdmin):
+    fields = ('topology', 'client', 'message_type', 'message_id', 'message_data',)
+    raw_id_fields = ('topology', 'client', 'message_type',)
+
+
+admin.site.register(TopologyHistory, TopologyHistoryAdmin)
+
+
+class MessageTypeAdmin(admin.ModelAdmin):
+    fields = ('name',)
+    raw_id_fields = ()
+
+
+admin.site.register(MessageType, MessageTypeAdmin)
--- prototype/models.py
+++ prototype/models.py
@@ -4,17 +4,11 @@ from django.db import models
 class Device(models.Model):
 
     device_id = models.AutoField(primary_key=True,)
-
     topology = models.ForeignKey('Topology',)
-
     name = models.CharField(max_length=200, )
-
     x = models.IntegerField()
-
     y = models.IntegerField()
-
     id = models.IntegerField()
-
     type = models.CharField(max_length=200, )
 
     def __unicode__(self):
@@ -24,23 +18,38 @@ class Device(models.Model):
 class Link(models.Model):
 
     link_id = models.AutoField(primary_key=True,)
-
     from_device = models.ForeignKey('Device',  related_name='+', )
-
     to_device = models.ForeignKey('Device',  related_name='+', )
 
 
 class Topology(models.Model):
 
     topology_id = models.AutoField(primary_key=True,)
-
     name = models.CharField(max_length=200, )
-
     scale = models.FloatField()
-
     panX = models.FloatField()
-
     panY = models.FloatField()
 
     def __unicode__(self):
         return self.name
+
+
+class Client(models.Model):
+
+    client_id = models.AutoField(primary_key=True,)
+
+
+class TopologyHistory(models.Model):
+
+    topology_history_id = models.AutoField(primary_key=True,)
+    topology = models.ForeignKey('Topology',)
+    client = models.ForeignKey('Client',)
+    message_type = models.ForeignKey('MessageType',)
+    message_id = models.IntegerField()
+    message_data = models.TextField()
+
+
+class MessageType(models.Model):
+
+    message_type_id = models.AutoField(primary_key=True,)
+    name = models.CharField(max_length=200, )
