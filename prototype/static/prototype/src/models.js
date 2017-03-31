d2948441fc5fc2edbf68c5c5c8f25a3ac2efba4a
Ben Thomasson <bthomass@ansible.com>
2017-03-31 11:10:32 -0400

    Calculate the midpoint and intersection of transitions to states

--- prototype/static/prototype/src/models.js
+++ prototype/static/prototype/src/models.js
@@ -9,6 +9,7 @@ function State(id, name, x, y) {
     this.y = y;
     this.height = 50;
     this.width = 50;
+    this.size = 50;
     this.type = 'state';
     this.selected = false;
     this.remote_selected = false;
@@ -53,6 +54,22 @@ exports.Transition = Transition;
 Transition.prototype.toJSON = function () {
     return {to_state: this.to_state.id,
             from_state: this.from_state.id};
+};
+
+Transition.prototype.slope = function () {
+    var x1 = this.from_state.x;
+    var y1 = this.from_state.y;
+    var x2 = this.to_state.x;
+    var y2 = this.to_state.y;
+    return Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI + 180;
+};
+
+Transition.prototype.length = function () {
+    var x1 = this.from_state.x;
+    var y1 = this.from_state.y;
+    var x2 = this.to_state.x;
+    var y2 = this.to_state.y;
+    return Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
 };
 
 function Button(name, x, y, width, height, callback) {
