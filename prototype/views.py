9104ae087f43018e5f35f25d85bf1cd079accab3
Ben Thomasson <bthomass@ansible.com>
2017-03-31 17:02:41 -0400

    Add upload form

--- prototype/views.py
+++ prototype/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 import yaml
 from prototype.models import FiniteStateMachine, State, Transition
 
@@ -10,6 +11,10 @@ from django import forms
 
 class FSMForm(forms.Form):
     finite_state_machine_id = forms.IntegerField()
+
+
+class UploadFileForm(forms.Form):
+    file = forms.FileField()
 
 # Create your views here.
 
@@ -42,6 +47,7 @@ def download(request):
     if form.is_valid():
         finite_state_machine_id = form.cleaned_data['finite_state_machine_id']
         fsm = FiniteStateMachine.objects.get(pk=finite_state_machine_id)
+        data['name'] = fsm.name
         data['finite_state_machine_id'] = fsm.pk
         data['states'] = map(transform_state, list(State.objects
                                                         .filter(finite_state_machine_id=finite_state_machine_id)
@@ -62,5 +68,42 @@ def download(request):
         return HttpResponse(form.errors)
 
 
+def upload_fsm(data):
+    fsm = FiniteStateMachine()
+    fsm.name = data.get('name', data.get("app", "fsm"))
+    fsm.save()
+    states = []
+    transitions = []
+    for i, state in enumerate(data.get('states', [])):
+        new_state = State(finite_state_machine_id=fsm.pk,
+                          name=state.get('label'),
+                          id=i + 1,
+                          x=state.get('x', 0),
+                          y=state.get('y', 0))
+        states.append(new_state)
+    State.objects.bulk_create(states)
+    states_map = dict(State.objects
+                           .filter(finite_state_machine_id=fsm.pk)
+                           .values_list("name", "pk"))
+    for transition in data.get('transitions', []):
+        new_transition = Transition(label=transition['label'],
+                                    from_state_id=states_map[transition['from_state']],
+                                    to_state_id=states_map[transition['to_state']])
+        transitions.append(new_transition)
+
+    Transition.objects.bulk_create(transitions)
+    return fsm.pk
+
+
 def upload(request):
-    return HttpResponse("")
+    if request.method == 'POST':
+        print request.POST
+        print request.FILES
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = yaml.load(request.FILES['file'].read())
+            fsm_id = upload_fsm(data)
+            return HttpResponseRedirect('/static/prototype/index.html#!?finite_state_machine_id={0}'.format(fsm_id))
+    else:
+        form = UploadFileForm()
+    return render(request, 'prototype/upload.html', {'form': form})
