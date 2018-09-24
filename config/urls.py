9ca050a38e5b172eca421ce55d47e3b29fee2563
Ben Thomasson <ben.thomasson@gmail.com>
2018-09-24 17:01:18 -0400

    Update urls for react app

--- config/urls.py
+++ config/urls.py
@@ -6,20 +6,9 @@ from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/",
-        TemplateView.as_view(template_name="pages/about.html"),
-        name="about",
-    ),
+    path("", TemplateView.as_view(template_name="prototype/index.html"), name="index"),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
-    # User management
-    path(
-        "users/",
-        include("fsm_designer_svg.users.urls", namespace="users"),
-    ),
-    path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
     path("prototype/",
         include("prototype.urls", namespace="prototype")
