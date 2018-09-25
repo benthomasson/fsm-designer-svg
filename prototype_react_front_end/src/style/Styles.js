22101c27f4f539f5ed712bd8a44a53a38bc044ce
Ben Thomasson <ben.thomasson@gmail.com>
2018-09-25 15:10:20 -0400

    Adds Styles

---
+++ prototype_react_front_end/src/style/Styles.js
@@ -0,0 +1,12 @@
+
+import Colors from '../style/Colors';
+
+var pathStyle = {
+  fill: Colors['darkWidgetDetail'],
+  stroke: Colors['darkWidgetDetail'],
+};
+
+var textStyle = {
+  fill: Colors['darkWidgetDetail'],
+};
+export {textStyle, pathStyle};
