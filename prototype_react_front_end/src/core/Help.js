0611ad44d852ac18bb53026f6cf58162f78c84f9
Ben Thomasson <ben.thomasson@gmail.com>
2018-09-27 10:14:42 -0400

    Cleaning up hot keys help

--- prototype_react_front_end/src/core/Help.js
+++ prototype_react_front_end/src/core/Help.js
@@ -11,19 +11,14 @@ class Upload extends Component {
         this.props.showHelp ?
           <g transform={'translate(' + this.props.x + ',' + this.props.y + ')'}>
               <text x='0' y={next_i()}>Hot Keys:</text>
+              <text x='0' y={next_i()}>s - New state</text>
+              <text x='0' y={next_i()}>t - New transition</text>
+              <text x='0' y={next_i()}>f - New FSM</text>
+              <text x='0' y={next_i()}>0 - Reset scale and pan</text>
               <text x='0' y={next_i()}>h - Toggle help</text>
               <text x='0' y={next_i()}>d - Toggle debug</text>
               <text x='0' y={next_i()}>p - Toggle pointer</text>
               <text x='0' y={next_i()}>b - Toggle buttons</text>
-              <text x='0' y={next_i()}>s - New state</text>
-              <text x='0' y={next_i()}>t - New transition</text>
-              <text x='0' y={next_i()}>f - New FSM</text>
-              <text x='0' y={next_i()}>c - New channel</text>
-              <text x='0' y={next_i()}>0 - Reset scale and pan</text>
-              <text x='0' y={next_i()}>Space - Toggle Pause</text>
-              <text x='0' y={next_i()}>r - Restart replay</text>
-              <text x='0' y={next_i()}>j - Step replay back</text>
-              <text x='0' y={next_i()}>k - Step replay forward</text>
           </g>
       : null
     );
